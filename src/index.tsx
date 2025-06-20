a1665bb994181ca616ded487abcd8ccdfe5d6772
Leonardo Rodrigues <leorodriguesoffice@gmail.com>
2025-06-19 22:04:03 -0300

    Ajust in useTransactions

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,7 @@ import ReactDOM from "react-dom";
 import { App } from './App';
 import { makeServer } from './services/mirage'
 
-if (process.env.NODE_ENV === 'development') {
-  makeServer()
-}
+makeServer()
 
 ReactDOM.render(
   <React.StrictMode>
